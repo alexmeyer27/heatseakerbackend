4f899081da0ead720a256cf33d2976519fb3b09a
Alex <alexmeyer@gmail.com>
2025-02-19 14:38:25 -0700

    init, env variables added

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,4 +5,6 @@ dotenv.config();
 export default {
   port: Number(process.env.PORT) || 8080,
   apiKey: process.env.API_KEY || "default-api-key",
+  alertSlackWebhookUrl: process.env.ALERT_SLACK_WEBHOOK_URL || "",
+  errorSlackWebhookUrl: process.env.ERROR_SLACK_WEBHOOK_URL || "",
 };
