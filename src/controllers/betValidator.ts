cb3b643d9770fc31f12bf3cc6380d596df582906
Alex <alexmeyer@gmail.com>
2025-03-12 14:20:24 -0600

    new validation

--- src/controllers/betValidator.ts
+++ src/controllers/betValidator.ts
@@ -1,24 +1,23 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const betSchema = Joi.object({
-  bets: Joi.array()
-    .items(
-      Joi.object({
-        trackCode: Joi.string().required(),
-        raceNumber: Joi.number().integer().positive().required(),
-        horseNumber: Joi.alternatives()
-          .try(Joi.string(), Joi.number())
-          .required(),
-        betAmount: Joi.string().required(),
-        placeBetAmount: Joi.string().optional(),
-        exactaBetAmount: Joi.string().optional(),
-        exactaHorseNumber: Joi.number().positive().optional(),
-        type: Joi.string().valid("WIN", "PLACE", "EXACTA").required(),
-      })
-    )
-    .min(1)
-    .required(),
-  betType: Joi.string()
-    .valid("aBet", "bBet", "cBet", "dBet", "eBet", "fBet", "gBet")
-    .required(),
+  bets: Joi.array().items(
+    Joi.object({
+      trackCode: Joi.string().required(),
+      raceNumber: Joi.number().integer().required(),
+      betType: Joi.string().valid('WIN', 'PLACE', 'SHOW', 'EXACTA', /* other bet types */).required(),
+      betCombination: Joi.when('betType', {
+        is: 'EXACTA',
+        then: Joi.string().pattern(/^(\d+(-\d+)+)$/).required(), // Must be "X-Y" format for Exacta
+        otherwise: Joi.optional(),
+      }),
+      dollarAmount: Joi.number().precision(2).required(),
+      comboType: Joi.when('betType', {
+        is: 'EXACTA',
+        then: Joi.string().valid('WHEEL', 'KEY', 'BOX', 'KEY-BOX', 'POWER-BOX').required(),
+        otherwise: Joi.optional(),
+      }),
+    })
+  ).required(),
+  betType: Joi.string().required(),
 });
